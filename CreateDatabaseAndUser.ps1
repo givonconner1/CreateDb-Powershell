782476c16d34c4222dfffa8e4b2d6155cf9eef2c
Tom Ellis <ellist@parliament.uk>
2017-05-26 10:31:36 +0100

    Powershell scripts for db creation

---
+++ CreateDatabaseAndUser.ps1
@@ -0,0 +1,75 @@
+﻿##Paramaters
+Param(
+	[Parameter(Mandatory=$true)][string]$applicationName,
+    [string]$environmentDev = 'DEVCI',
+    [string]$environmentQa  = 'QA',
+    [string]$serverIpDev = '.',
+    [string]$serverIpQa = '.',
+    [string]$userName = 'sa',
+    [string]$filePath = (Get-Item -Path ".\" -Verbose).FullName
+)
+
+#Password Prompt
+$credential = Get-Credential "$userName"  #Change this when executing remotly
+$pass = $credential.GetNetworkCredential().password 
+
+#Create the Database name
+Function CreateDbName($environment, $applicationName)
+{
+    return $environment + '_' + $applicationName -replace '\s',''
+}
+
+#Create the db username
+Function CreateUsername($environment, $applicationName)
+{
+    return "$environment-$applicationName user" -replace '\s',''
+}
+
+#Create the db passwords (if I assign a variable of $sqlPassword to the GenerateDbPassword function can I merger SaveDb and this function?)
+Function GenerateDbPassword()
+{
+    return -join ((65..90) + (97..122) | Get-Random -Count 28 | % {[char]$_})
+
+}
+
+#Save the database password
+Function saveDbPassword($user, $sqlPassword)
+{
+    Out-File -Append -FilePath "$filePath\GeneratePasswords.txt" -InputObject "$user $sqlPassword" #Outputs the username and password
+}
+
+
+#Pass variables into sql script and execute on the server
+Function invokeSql($serverIp, $userName, $pass, $filePath, $sqlParam)
+{
+    invoke-sqlcmd -ServerInstance $serverIp -Username $userName -Password $pass -inputfile "$filePath\CreateSqlUser.sql" -variable $sqlParam
+}
+
+##Variables
+$devDb = CreateDbName $environmentDev $applicationName
+$qaDb = CreateDbName $environmentQa $applicationName
+
+$devUser = CreateUsername $environmentDev.ToLower().Trim() $applicationName.ToLower()
+$qaUser = CreateUsername $environmentQa.ToLower().Trim() $applicationName.ToLower()
+
+$devPwd = GenerateDbPassword
+$qaPwd = GenerateDbPassword
+
+
+$sqlParametersDev = @("user = $devUser", "dbName = $devDb", "sqlPassword = $devPwd")
+$sqlParametersQa = @("user = $qaUser", "dbName = $qaDb", "sqlPassword = $qaPwd")
+
+
+invokeSql $serverIpDev $userName $pass $filePath $sqlParametersDev
+invokeSql $serverIpQ $userName $pass $filePath $sqlParametersQa
+
+saveDbPassword $devUser $devPwd
+saveDbPassword $qaUser $qaPwd
+
+Write-Host DevCi database: $devDb 
+Write-Host Username: $devUser
+Write-Host Password: $devPwd
+
+Write-Host Qa database: $qaDb
+Write-Host Username: $qaUser
+Write-Host Password: $qaPwd
---
+++ CreateSqlUser.sql
@@ -0,0 +1,156 @@
+-- Replace $(application) for the username. This MUST be all lowercase with NO spaces
+-- Replace $(dbName) with the name of the db
+-- Replace the $(Environment) with either DEVCI or QA in lowercase
+
+
+--REPLACE C: with F: when transferring to Azure
+
+CREATE DATABASE [$(dbName)]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'$(dbName)', FILENAME = N'C:\Data\$(dbName).mdf' , SIZE = 93184KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+ LOG ON 
+( NAME = N'$(dbName)_log', FILENAME = N'C:\Log\$(dbName)_log.ldf' , SIZE = 47616KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+GO
+
+ALTER DATABASE [$(dbName)] SET COMPATIBILITY_LEVEL = 120
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [$(dbName)].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [$(dbName)] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [$(dbName)] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [$(dbName)] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [$(dbName)] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [$(dbName)] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [$(dbName)] SET AUTO_CLOSE OFF 
+GO
+
+ALTER DATABASE [$(dbName)] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [$(dbName)] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [$(dbName)] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [$(dbName)] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [$(dbName)] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [$(dbName)] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [$(dbName)] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [$(dbName)] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [$(dbName)] SET  DISABLE_BROKER 
+GO
+
+ALTER DATABASE [$(dbName)] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [$(dbName)] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [$(dbName)] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [$(dbName)] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [$(dbName)] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [$(dbName)] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [$(dbName)] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [$(dbName)] SET RECOVERY SIMPLE 
+GO
+
+ALTER DATABASE [$(dbName)] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [$(dbName)] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [$(dbName)] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [$(dbName)] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [$(dbName)] SET TARGET_RECOVERY_TIME = 0 SECONDS 
+GO
+
+ALTER DATABASE [$(dbName)] SET DELAYED_DURABILITY = DISABLED 
+GO
+
+ALTER DATABASE [$(dbName)] SET  READ_WRITE 
+GO
+
+
+
+
+
+--Undo this once the db is creating. 
+
+
+
+
+
+
+USE [master]
+GO
+
+CREATE LOGIN [$(user)] WITH PASSWORD=N'$(sqlPassword)', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+GO
+
+--Add the Connect SQL function
+USE [master]
+GO
+
+GRANT CONNECT SQL TO [$(user)]
+GO
+
+--Create db user 
+	--This cannot be switched on until the db script is run, to create the db. 
+	
+use $(dbName)
+GO
+CREATE USER [$(user)] FOR LOGIN [$(user)]
+GO
+
+	--set user as db_owner
+
+
+USE $(dbName)
+GO
+ALTER ROLE [db_owner] ADD MEMBER [$(user)]
+GO
+
+
+
