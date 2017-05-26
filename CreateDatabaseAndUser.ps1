9ac91d8b1634a21580083c85ffbae4e5be969c94
Tom Ellis <ellist@parliament.uk>
2017-05-26 13:58:30 +0100

    Creating a Powershell interactive menu

--- CreateDatabaseAndUser.ps1
+++ CreateDatabaseAndUser.ps1
@@ -1,7 +1,4 @@
-﻿##Script to create db
-
-##Paramaters
-Param(
+﻿Param(
 	[Parameter(Mandatory=$true)][string]$applicationName,
     [string]$environmentDev = 'DEVCI',
     [string]$environmentQa  = 'QA',
@@ -14,6 +11,20 @@ Param(
 #Password Prompt
 $credential = Get-Credential "$userName"  #Change this when executing remotly
 $pass = $credential.GetNetworkCredential().password 
+
+function Show-Menu
+{
+    param (
+        [string]$Title = 'Database Deployment & User Creation'
+    )
+    clear
+    Write-Host "================= $Title ================="
+
+    Write-Host "1: Press '1' to create a DevCi database"
+    Write-Host "2: Press '2' to create a Qa database"
+    Write-Host "3: Press '3' to create a DevCi and Qa Database"
+    Write-Host "Q: Press 'Q' to exit the program"
+}
 
 #Create the Database name
 Function CreateDbName($environment, $applicationName)
@@ -47,31 +58,77 @@ Function invokeSql($serverIp, $userName, $pass, $filePath, $sqlParam)
     invoke-sqlcmd -ServerInstance $serverIp -Username $userName -Password $pass -inputfile "$filePath\CreateSqlUser.sql" -variable $sqlParam
 }
 
-##Variables
-$devDb = CreateDbName $environmentDev $applicationName
-$qaDb = CreateDbName $environmentQa $applicationName
+#This is the do until loop
+do 
+{
+    Show-Menu
+    $input = Read-Host "Please make a selection"
+    switch($input)
+    {
+        '1'{
+            clear
+            'You chose to create a DevCi database'
+            $devDb = CreateDbName $environmentDev $applicationName
+            $devUser = CreateUsername $environmentDev.ToLower().Trim() $applicationName.ToLower()
+            $devPwd = GenerateDbPassword
+            $sqlParametersDev = @("user = $devUser", "dbName = $devDb", "sqlPassword = $devPwd")
+            invokeSql $serverIpDev $userName $pass $filePath $sqlParametersDev
+            saveDbPassword $devUser $devPwd
+            Write-Host DevCi database: $devDb 
+            Write-Host Username: $devUser
+            Write-Host Password: $devPwd
 
-$devUser = CreateUsername $environmentDev.ToLower().Trim() $applicationName.ToLower()
-$qaUser = CreateUsername $environmentQa.ToLower().Trim() $applicationName.ToLower()
+        }
+        '2'{
+            clear
+            'You chose to create a Qa database'
+            $qaDb = CreateDbName $environmentQa $applicationName
+            $qaUser = CreateUsername $environmentQa.ToLower().Trim() $applicationName.ToLower()
+            $qaPwd = GenerateDbPassword
+            $sqlParametersQa = @("user = $qaUser", "dbName = $qaDb", "sqlPassword = $qaPwd")
+            invokeSql $serverIpQ $userName $pass $filePath $sqlParametersQa
+            saveDbPassword $qaUser $qaPwd
 
-$devPwd = GenerateDbPassword
-$qaPwd = GenerateDbPassword
+            Write-Host Qa database: $qaDb
+            Write-Host Username: $qaUser
+            Write-Host Password: $qaPwd
+
+        }
+        '3'{
+            clear
+            'You chose to create a DevCi and Qa Database'
+            ##Variables
+            $devDb = CreateDbName $environmentDev $applicationName
+            $qaDb = CreateDbName $environmentQa $applicationName
+
+            $devUser = CreateUsername $environmentDev.ToLower().Trim() $applicationName.ToLower()
+            $qaUser = CreateUsername $environmentQa.ToLower().Trim() $applicationName.ToLower()
+
+            $devPwd = GenerateDbPassword
+            $qaPwd = GenerateDbPassword
 
 
-$sqlParametersDev = @("user = $devUser", "dbName = $devDb", "sqlPassword = $devPwd")
-$sqlParametersQa = @("user = $qaUser", "dbName = $qaDb", "sqlPassword = $qaPwd")
+            $sqlParametersDev = @("user = $devUser", "dbName = $devDb", "sqlPassword = $devPwd")
+            $sqlParametersQa = @("user = $qaUser", "dbName = $qaDb", "sqlPassword = $qaPwd")
 
+            invokeSql $serverIpDev $userName $pass $filePath $sqlParametersDev
+            invokeSql $serverIpQ $userName $pass $filePath $sqlParametersQa
 
-invokeSql $serverIpDev $userName $pass $filePath $sqlParametersDev
-invokeSql $serverIpQ $userName $pass $filePath $sqlParametersQa
+            saveDbPassword $devUser $devPwd
+            saveDbPassword $qaUser $qaPwd
 
-saveDbPassword $devUser $devPwd
-saveDbPassword $qaUser $qaPwd
+            Write-Host DevCi database: $devDb 
+            Write-Host Username: $devUser
+            Write-Host Password: $devPwd
 
-Write-Host DevCi database: $devDb 
-Write-Host Username: $devUser
-Write-Host Password: $devPwd
-
-Write-Host Qa database: $qaDb
-Write-Host Username: $qaUser
-Write-Host Password: $qaPwd
+            Write-Host Qa database: $qaDb
+            Write-Host Username: $qaUser
+            Write-Host Password: $qaPwd
+        }
+        'q'{
+            return
+        }
+    }
+    pause
+}
+until ($input -eq 'q')
